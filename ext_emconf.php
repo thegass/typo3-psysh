c21265904c15c40623999765c1979a226480fbc8
Oliver Gassner <og@plan2.net>
2016-12-01 17:23:23 +0100

    tweaks

---
+++ ext_emconf.php
@@ -0,0 +1,43 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "kuoni_stammdaten".
+ *
+ * Auto generated 18-09-2013 14:01
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'PsySH',
+	'description' => 'PsySH for Typo3',
+	'category' => 'dev',
+	'author' => 'Oliver Gassner',
+	'author_email' => 'og@plan2.net',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => 1,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.1',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+);
